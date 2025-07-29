13596c0ee3360f5c096f41873c37425e60de5d69
srsi3 <ssaldanha03@gmail.com>
2025-07-29 12:02:22 -0400

    update meshes to remove bugs

--- hello_wgpu/src/mesh.rs
+++ hello_wgpu/src/mesh.rs
@@ -1,44 +1,60 @@
+use cgmath::{Matrix4, Vector3};
+use wgpu::util::DeviceExt; // for create_buffer_init
+use bytemuck::{Pod, Zeroable};
+
 #[repr(C)]
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Pod, Zeroable)]
 pub struct Vertex {
-    position: [f32; 3], // x, y, z
-    color: [f32; 4],    // r, g, b, a
+    pub position: [f32; 3], // @location(0)
+    pub color:    [f32; 4], // @location(1)
 }
 
 impl Vertex {
-    const ATTRIBUTES: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0 => Float32, 1 => Float64];
+    pub const ATTRIBUTES: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![
+        0 => Float32x3, // position
+        1 => Float32x4, // color
+    ];
+
+    pub const fn layout() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBUTES,
+        }
+    }
 }
 
-struct Mesh {
-    vertices: Vec<Vertex>,
-    indices: Vec<u16>,  // Indices for indexed drawing (triangle list)
-    vertex_buffer: wgpu::Buffer,
-    index_buffer: wgpu::Buffer,
+pub struct Mesh {
+    pub vertex_buffer: wgpu::Buffer,
+    pub index_buffer:  wgpu::Buffer,
+    pub index_count:   u32,
 }
 
 pub fn create_cube(device: &wgpu::Device) -> Mesh {
-    let vertices = vec![
-        // Front face
+    let vertices: Vec<Vertex> = vec![
+        // Front (red)
         Vertex { position: [-0.5, -0.5,  0.5], color: [1.0, 0.0, 0.0, 1.0] },
         Vertex { position: [ 0.5, -0.5,  0.5], color: [1.0, 0.0, 0.0, 1.0] },
         Vertex { position: [ 0.5,  0.5,  0.5], color: [1.0, 0.0, 0.0, 1.0] },
         Vertex { position: [-0.5,  0.5,  0.5], color: [1.0, 0.0, 0.0, 1.0] },
 
-        // Back face
+        // Back (green)
         Vertex { position: [-0.5, -0.5, -0.5], color: [0.0, 1.0, 0.0, 1.0] },
         Vertex { position: [ 0.5, -0.5, -0.5], color: [0.0, 1.0, 0.0, 1.0] },
         Vertex { position: [ 0.5,  0.5, -0.5], color: [0.0, 1.0, 0.0, 1.0] },
         Vertex { position: [-0.5,  0.5, -0.5], color: [0.0, 1.0, 0.0, 1.0] },
     ];
 
+    // Triangle list (two per face)
     let indices: Vec<u16> = vec![
-        0, 1, 2, 2, 3, 0, // Front
-        4, 5, 6, 6, 7, 4, // Back
-        0, 1, 5, 5, 4, 0, // Bottom
-        1, 2, 6, 6, 5, 1, // Right
-        2, 3, 7, 7, 6, 2, // Top
-        3, 0, 4, 4, 7, 3, // Left
+        0, 1, 2,  2, 3, 0, // Front
+        4, 5, 6,  6, 7, 4, // Back
+        0, 1, 5,  5, 4, 0, // Bottom
+        1, 2, 6,  6, 5, 1, // Right
+        2, 3, 7,  7, 6, 2, // Top
+        3, 0, 4,  4, 7, 3, // Left
     ];
+    let index_count = indices.len() as u32;
 
     let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("Vertex Buffer"),
@@ -53,24 +69,25 @@ pub fn create_cube(device: &wgpu::Device) -> Mesh {
     });
 
     Mesh {
-        vertices,
-        indices,
         vertex_buffer,
         index_buffer,
+        index_count,
     }
 }
 
-
-pub fn create_city_block(device: &wgpu::Device, grid_size: usize) -> Vec<Mesh> {
+/// Builds a grid of cubes; each entry is (Mesh, model_matrix)
+pub fn create_city_block(device: &wgpu::Device, grid_size: usize) -> Vec<(Mesh, Matrix4<f32>)> {
     let mut meshes = Vec::new();
-
     for x in 0..grid_size {
         for z in 0..grid_size {
             let mesh = create_cube(device);
-            let translation = cgmath::Matrix4::from_translation(cgmath::Vector3::new(x as f32 * 2.0, 0.0, z as f32 * 2.0));
+            let translation = Matrix4::from_translation(Vector3::new(
+                (x as f32) * 2.0,
+                0.0,
+                (z as f32) * 2.0,
+            ));
             meshes.push((mesh, translation));
         }
     }
-
     meshes
 }
